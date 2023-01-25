d8c724d265fb6bb7dde77f2a202230ea50a7521c
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-01-25 23:43:18 +0330

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiBlazorUIBestPractices;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        BlazorWebViewHandler.BlazorWebViewMapper.AppendToMapping("CustomBlazorWebViewMapper", (handler, view) =>
+        {
+#if WINDOWS
+            //Workaround for WinUI splash screen
+            if (AppInfo.Current.RequestedTheme == AppTheme.Dark)
+            {
+                handler.PlatformView.DefaultBackgroundColor = Microsoft.UI.Colors.Black;
+            }
+#endif
+
+#if IOS || MACCATALYST
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+            handler.PlatformView.Opaque = false;
+#endif
+
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#endif
+        });
+    }
+}
