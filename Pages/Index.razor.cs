6499496017c3716e69da398093e27f752521fb71
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-02-15 02:05:36 +0330

    Add Window size event listener

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MauiBlazorUIBestPractices.Pages;
+
+public partial class Index : ComponentBase
+{
+    public int WindowWidth { get; set; }
+    private string _resizeEventListenerId = string.Empty;
+    private DotNetObjectReference<Index>? _dotnetObjectReference;
+
+    [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _dotnetObjectReference = DotNetObjectReference.Create(this);
+        await base.OnInitializedAsync();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("UpdateWindowWidth", _dotnetObjectReference);
+            await InitWindowWidthListener();
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
+    [JSInvokable]
+    public void UpdateWindowWidth(int windowWidth)
+    {
+        WindowWidth = windowWidth;
+        StateHasChanged();
+    }
+
+    private async Task InitWindowWidthListener()
+    {
+        _resizeEventListenerId = Guid.NewGuid().ToString();
+        await JSRuntime.InvokeVoidAsync("AddWindowWidthListener", _dotnetObjectReference, _resizeEventListenerId);
+    }
+}
---
+++ wwwroot/js/windowSize.js
@@ -0,0 +1,16 @@
+﻿let windowEventListeners = {};
+
+function AddWindowWidthListener(objReference, id) {
+    let eventListener = () => UpdateWindowWidth(objReference);
+    window.addEventListener("resize", eventListener);
+    windowEventListeners[id] = eventListener;
+}
+
+function RemoveWindowWidthListener(id) {
+    window.removeEventListener("resize", windowEventListeners[id]);
+    delete windowEventListeners[id];
+}
+
+function UpdateWindowWidth(objReference) {
+    objReference.invokeMethodAsync("UpdateWindowWidth", window.innerWidth);
+}
